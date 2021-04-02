6bd6b429f682a485ee70ecee786b1aedd3a0b751
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-04-02 11:43:35 -0300

    python for throughput

---
+++ src/teste.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h>
+
+int move_student(int n, int *tentatives, ){ 
+    return 0; 
+}
+
+void main(){ 
+    int n ; // size     
+    int **Maze=  (int**)malloc(n*sizeof(int*)); 
+    for(int i = 0 ; i < n ; i++){ 
+        Maze[i] = (int*)malloc(n*sizeof(int)); 
+    }  
+
+}
