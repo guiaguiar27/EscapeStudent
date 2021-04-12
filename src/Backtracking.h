7db791c31693be9d27bf312d67514955beb7c26c
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-04-12 10:14:42 -0300

    almost done

--- src/Backtracking.h
+++ src/Backtracking.h
@@ -1,3 +1,10 @@
+/*
+   *    ============= TP0 - PAA ==============  
+   *    =       Cláudio Barbosa  - 3492      = 
+   *    =       Guilherme Aguiar - 3496      =
+   *    ======================================
+*/
+
 #include <stdio.h>
 #include <stdlib.h>   
 #define True 1 
@@ -24,67 +31,74 @@ typedef struct Node{
 }Node_t; 
 typedef Node_t DoorList; 
 
-// linked list to store the solution 
  
 
 /**   
-* \brief   
+* \brief  find the initial position   
 **/
 MazePosition *findPosition(int **maze, int line, int column); 
 /**   
-* \brief   
+* \brief   allocate memory to position Data structure 
 **/
 MazePosition *allocate_position(); 
 /**   
-* \brief   
+* \brief   show the current position
 **/
 void  show_position(int line, int column); 
 /**   
-* \brief   
+* \brief   checks if the position is reachable
 **/
 int verify_position(int **maze, int line, int column,int *keys, DoorList *list, int Tline, int Tcolumn); 
 /**   
-* \brief   
+* \brief   the main function, adopt backtracking solution  
 **/ 
 int initStudent(int **maze, int line, int column,int *keys, MazePosition *position,  int ** solution, int *count, DoorList *list,int *direction);
 //solution   
 /**   
-* \brief   
+* \brief   mark off the position  in the matrix solution 
 **/ 
 int markOFF_position_solution(int **solution, int line, int column); 
 /**   
-* \brief   
+* \brief   mark the current position in the matrix solution   
 **/ 
 int mark_position_solution(int **solution, int line, int column); 
 /**   
-* \brief   
+* \brief   checks if the position is already computed in the solution
 **/ 
 int verify_solution(int **solution, int line, int column); 
 /**   
-* \brief   
+* \brief   init the solution matrix 
 **/ 
 int **initSolution(int line, int column); 
 /**   
-* \brief   
+* \brief   show the final solution 1 indicate the path 
 **/ 
 void show_solution_maze(int **solution, int line, int column); 
 /**   
-* \brief   
+* \brief   pre function to start the maze solution
 **/  
 void initBacktrackingMaze(int **maze, int line, int column, int *keys);  
 /**   
-* \brief   
+* \brief   allocate the door list 
 **/   
 void allocate_list(DoorList **d);
 /**   
-* \brief   
+* \brief checks if the current door is already computed in the list    
 **/   
-
 int verify_door(DoorList *Dl , int line , int column);  
 /**   
-* \brief   
+* \brief   show the next studdent direction 
 **/   
 void show_direction(int *dir);
+/**   
+* \brief   insert a new door 
+**/   
 int insert_door(DoorList **dl, int line, int column); 
-int remove_door(DoorList **dl, int line, int column);  
+/**   
+* \brief   remove a door from the list 
+**/   
+int remove_door(DoorList **dl, int line, int column);   
+/**   
+* \brief   show all listed doors
+**/  
 void show_doors(DoorList **DL);
--- src/menu.h
+++ src/menu.h
@@ -1,6 +1,7 @@
 /*
    *    ============= TP0 - PAA ==============  
-   *    =       Cláudio Barbosa  - 3492      =
+   *    =       Cláudio Barbosa  - 3492      = 
+   *    =       Guilherme Aguiar - 3496      =
    *    ======================================
 */
 
@@ -11,8 +12,20 @@
 
 
 typedef enum { F, T } boolean;
-
+/**   
+* \brief  init the menu    
+**/
 void menu (); 
+/**   
+* \brief  load the file and fill the maze's matrix 
+**/
 int **loadFile(char *file, int *Pline , int *Pcolumn, int *Pkeys ); 
+/**   
+* \brief  show filled  matrix
+**/
+
 void show_maze(int **matrixLab, int lines, int columns); 
+/**
+* \brief deallocate the already used matrix
+**/
 void liberaLabirinto(int **matrizLab, int linha);  
